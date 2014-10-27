a00108124f008dff6a57ebc8d0d64435d07beb3e
Serge Majkic <serge.majkic@D2L.com>
2014-10-27 14:21:31 -0400

    First attempt at publishing comments to GitHub.

---
+++ sendComment.py
@@ -0,0 +1,20 @@
+import urllib
+import urllib2
+import os
+import sys
+import json
+
+repo_slug = os.environ['TRAVIS_REPO_SLUG']
+commit_sha = os.environ['COMMIT_SHA']
+token = os.environ['GITHUB_TOKEN']
+userAgent = 'peanut-gallery'
+githubUrl = 'https://api.github.com/repos/' + repo_slug + '/commits/' + commit_sha + '/comments'
+
+headers = {'Authorization': 'token ' + token, 'User-Agent' : userAgent}
+value = sys.argv[1]
+data = json.dumps( {"body": value } )
+
+request = urllib2.Request(githubUrl, data, headers)
+content = urllib2.urlopen(request).read()
+
+print content
